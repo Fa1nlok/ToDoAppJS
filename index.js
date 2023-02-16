2d441fc72c91d06bd7986b43de6f5c1b699da463
Fa1nlok <62547087+Fa1nlok@users.noreply.github.com>
2023-02-16 22:03:53 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+import {model} from './model'
+import {title, plan_title, plan_id, plans, button} from './templates'
+
+const $site = document.querySelector('#site');
+
+model.forEach(block =>{
+    let html = '';
+    if (block.type ==='title') {
+html = title(block)
+}
+    else if (block.type ==='plan_title') {
+      html = plan_title(block)
+    }
+    else if (block.type ==='plan_id') {
+      html = plan_id(block)
+    }
+    else if (block.type ==='plans') {
+      html = plans(block)
+    }
+    else if (block.type ==='button') {
+      html = button(block)
+    }
+    $site.insertAdjacentHTML('beforeend', html)
+  });
+
+ 
+  
+  
---
+++ model.js
@@ -0,0 +1,8 @@
+export const model = [
+  {type: 'title', value: 'ToDoApp'},
+  {type: 'plan_title', value: 'Go to learning JS'},
+  {type: 'plan_id', value: '1'},
+  {type: 'plans', value: 'Watch React.js course'},
+  {type: 'button', value: 'Add plan on day'},
+
+]
