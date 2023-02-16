11078ff59330d4b91fa88adf7d3bbd7718fd3e50
untermensch <untermenschH@protonmail.com>
2023-02-16 22:36:06 +0300

    add comments, and format code

--- model.js
+++ model.js
@@ -1,8 +1,15 @@
 export const model = [
-  {type: 'title', value: 'ToDoApp'},
-  {type: 'plan_title', value: 'Go to learning JS'},
-  {type: 'plan_id', value: '1'},
-  {type: 'plans', value: 'Watch React.js course'},
-  {type: 'button', value: 'Add plan on day'},
-
+  { type: 'title', value: 'ToDoApp' },
+  { type: 'plan_title', value: 'Go to learning JS' },
+  { type: 'plan_id', value: '1' },
+  { type: 'plans', value: 'Watch React.js course' },
+  { type: 'button', value: 'Add plan on day' },
 ]
+
+// export const model = [
+//   { title: 'ToDoApp' },
+//   { plan_title: 'Go to learning JS' },
+//   { plan_id: '1' },
+//   { plans: 'Watch React.js course' },
+//   { button: 'Add plan on day' },
+// ]
--- templates.js
+++ templates.js
@@ -1,28 +1,28 @@
-export function title(block){
+export function title(block) {
   const html = block.value.map(item => `<div class="col-sm"><h1>${item}</h1></div>`)
   return `<div class="row">
   ${html.join('')}
   </div>`
 }
-export function plan_title(block){
+export function plan_title(block) {
   const html = block.value.map(item => `<div class="col-sm"><p>${item}</p></div>`)
   return `<div class="row">
   ${html.join('')}
   </div>`
 }
-export function plan_id (block){
+export function plan_id(block) {
   const html = block.value.map(item => `<div class="col-sm"><p>${item}</p></div>`)
   return `<div class="row">
   ${html.join('')}
   </div>`
 }
-export function plans (block){
+export function plans(block) {
   const html = block.value.map(item => `<div class="col-sm"><a>${item}</a></div>`)
   return `<div class="row">
   ${html.join('')}
   </div>`
 }
-export function button(block){
+export function button(block) {
   const html = block.value.map(item => `<div class="col-sm"><button>${item}</button></div>`)
   return `<div class="row">
   ${html.join('')}
