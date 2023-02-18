b62276583c64d13c6bce8536b9bc7309629ce8a6
Fa1nlok <62547087+Fa1nlok@users.noreply.github.com>
2023-02-18 18:43:16 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
-import { model } from './model'
-import { title, plan_title, plan_id, plans, button } from './templates'
+const $content = document.querySelector("#content");
 
-const $site = document.querySelector('#site');
-
-model.forEach(block => {
-  let html = '';
-  if (block.type === 'title') {
-    html = title(block)
-  }
-  else if (block.type === 'plan_title') {
-    html = plan_title(block)
-  }
-  else if (block.type === 'plan_id') {
-    html = plan_id(block)
-  }
-  else if (block.type === 'plans') {
-    html = plans(block)
-  }
-  else if (block.type === 'button') {
-    html = button(block)
-  }
-  $site.insertAdjacentHTML('beforeend', html)
+let todos = JSON.parse(localStorage.getItem("todos")) || [];
+todos.forEach((e, index) => {
+  const todo = document.createElement("div");
+  const title = document.createElement("p");
+  title.innerHTML = `${index + 1} - ${e.title}`;
+  todo.appendChild(title);
+  $content.appendChild(todo);
 });
 
+function render(todoObj) {
+  const todo = document.createElement("div");
+  const title = document.createElement("p");
+  title.innerHTML = todoObj.title;
+  todo.appendChild(title);
+  $content.appendChild(todo);
+}
 
+function readInput() {
+  const inputTodo = document.getElementById("input").value;
+  return inputTodo;
+}
 
-
+function addTodo() {
+  const taskTitle = readInput();
+  if (taskTitle.length > 0) {
+    const todo = { title: taskTitle };
+    todos.push(todo);
+    localStorage.setItem("todos", JSON.stringify(todos));
+    render(todo);
+  }
+}
+window.addTodo = addTodo;
