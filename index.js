b4756aacd938edf91c262b64a31511b5a3c5aea8
Fa1nlok <62547087+Fa1nlok@users.noreply.github.com>
2023-02-23 17:34:39 +0300

    Adding delete buttons

--- index.js
+++ index.js
@@ -3,18 +3,32 @@ const $content = document.querySelector("#content");
 let todos = JSON.parse(localStorage.getItem("todos")) || [];
 todos.forEach((e, index) => {
   const todo = document.createElement("div");
+  todo.className = 'winForm'
   const title = document.createElement("p");
+  const delButton = document.createElement("button");
+  delButton.id =`${e.id}`
+  delButton.innerHTML = 'Delete Task'
   title.innerHTML = `${index + 1} - ${e.title}`;
   todo.appendChild(title);
+  todo.appendChild(delButton)
   $content.appendChild(todo);
 });
+
+function listener() {
+  delButton.onclick = delTodo(delButton.id)
+  console.log(delButton.id)
+}
 
 function render(todoObj) {
   const todo = document.createElement("div");
   const title = document.createElement("p");
+  const delButton = document.createElement("button");
   title.innerHTML = todoObj.title;
-  todo.appendChild(title);
+  todo.appendChild(title)
+  delButton.innerHTML = 'Delete Task'
+  todo.appendChild(delButton)
   $content.appendChild(todo);
+  
 }
 
 function readInput() {
@@ -24,11 +38,22 @@ function readInput() {
 
 function addTodo() {
   const taskTitle = readInput();
+  let index = todos.length
   if (taskTitle.length > 0) {
-    const todo = { title: taskTitle };
+    const todo = { 
+      id: index,  
+      title: taskTitle };
+      console.log(todo)
     todos.push(todo);
     localStorage.setItem("todos", JSON.stringify(todos));
     render(todo);
   }
 }
+ function delTodo() {
+ 
+  }
 window.addTodo = addTodo;
+window.delTodo = delTodo;
+window.listener = listener;
+
+  
