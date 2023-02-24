6c008000f86868479349300a5fff96e7010f3836
Fa1nlok <fa1nlok1@gmail.com>
2023-02-24 18:45:24 +0300

    Styling site
    Signed-off-by: Fa1nlok <fa1nlok1@gmail.com>

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const $content = document.querySelector("#content");
 
+const trashIcon = `<svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+<path d="M10 0C4.47 0 0 4.47 0 10C0 15.53 4.47 20 10 20C15.53 20 20 15.53 20 10C20 4.47 15.53 0 10 0ZM15 13.59L13.59 15L10 11.41L6.41 15L5 13.59L8.59 10L5 6.41L6.41 5L10 8.59L13.59 5L15 6.41L11.41 10L15 13.59Z"/>
+</svg>`;
 let todos = JSON.parse(localStorage.getItem("todos")) || [];
 
 todos.forEach((e, index) => {
   const todo = document.createElement("div");
-  todo.classList.add("winForm");
+  todo.classList.add("todo");
   const title = document.createElement("p");
+  
   const delButton = document.createElement("button");
+  const checkBox = document.createElement("input");
+  checkBox.type = `checkbox`;
+  checkBox.classList.add (`checkbox`);
   delButton.id = `${e.id}`;
-  delButton.innerHTML = "Delete Task";
+  delButton.classList.add(`delButton`);
+  delButton.innerHTML = trashIcon;
   delButton.onclick = function () {
     localStorage.removeItem("todos");
     todos = todos.filter((el) => {
@@ -19,7 +27,13 @@ todos.forEach((e, index) => {
     localStorage.setItem("todos", JSON.stringify(todos));
     $content.removeChild(todo);
   };
+  // checkBox.onchange = function() {
+  //   if (){
+
+  //   }
+  // }
   title.innerHTML = `${index + 1} - ${e.title}`;
+  todo.appendChild(checkBox);
   todo.appendChild(title);
   todo.appendChild(delButton);
   $content.appendChild(todo);
@@ -27,9 +41,15 @@ todos.forEach((e, index) => {
 
 function render(todoObj) {
   const todo = document.createElement("div");
+  todo.classList.add("todo");
   const title = document.createElement("p");
   const delButton = document.createElement("button");
+  const checkBox = document.createElement("input");
+  title.classList.add(`done`);
+  checkBox.type = `checkbox`;
+  checkBox.classList.add(`checkbox`);
   delButton.id = todos.length - 1;
+  delButton.classList.add(`delButton`);
   delButton.onclick = function (event) {
     const todoId = event.target.id;
     localStorage.removeItem("todos");
@@ -42,8 +62,9 @@ function render(todoObj) {
     $content.removeChild(todo);
   };
   title.innerHTML = `${todos.length} - ${todoObj.title}`;
+  todo.appendChild(checkBox);
   todo.appendChild(title);
-  delButton.innerHTML = "Delete Task";
+  delButton.innerHTML = trashIcon;
   todo.appendChild(delButton);
   $content.appendChild(todo);
 }
